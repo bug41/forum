17c8c4af365b1cfec5a4d2036e936a50704c1be5
bugkingus <bugkingus@gmail.com>
2023-05-10 09:04:44 +0900

    댓글 가져오기 기능

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -4,34 +4,50 @@ import { useEffect, useState } from "react"
 import dayjs from 'dayjs';
 
 export default function Comment(props){
+
+    const [count, setCount] = useState(0);
+
     let [comment, setComment] = useState('')
+    const [data, setData] = useState(null);
     const today = dayjs().format('YYYY-MM-DD HH:mm:ss')
 
     //client component 에서 로드할땐 여기서 한다
     useEffect(() => {
-        console.log("==============")
-        console.log(props._id)
-        console.log("==============")
-        fetch('/api/comment/list/_id='+props._id, {
-            method: 'GET'
-        })
-    },[])
+        console.log('useEffect 호출')
+        fetch('/api/comment/list?_id='+props._id, {method: 'GET'})
+        .then(
+            response => response.json()
+        ).then(data => {
+            console.log('useEffect 호출 후 data 가져옴')
+          setData(data); // 가져온 데이터를 상태 변수에 저장
+        });
+    },[count])
 
     return (
         <div>
-            <div>댓글</div>
+            <div>
+                {data ? (
+                    data.map((item, index) => (
+                     <div key={index}>{item.content}</div>
+                    ))
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
             <input onChange={(e)=>{setComment(e.target.value)}} />
             <button onClick={()=>{
-                console.log(comment);
+                //console.log(comment);
                 //fetch('/api/post/newComment', {method: 'POST', 
                 fetch('/api/comment/new', {method: 'POST', 
                     body: JSON.stringify({
                         "parent" : props._id,
                         "comment" : comment,
                         "created_by" : today
-                    }
-                    )
-                })
+                    })
+                }).then(
+                    setCount(count+1),
+                    console.log(count)
+                );
             }}>댓글입력</button>
         </div>
     )
--- pages/api/comment/list.js
+++ pages/api/comment/list.js
@@ -1,12 +1,12 @@
 import { connectDB } from "@/util/database"
 import { ObjectId } from "mongodb";
  export default async function handler(req,res) {
-
+    
     const {_id} = req.query;
 
     if(req.method == 'GET'){
         const db = (await connectDB).db("forum")
-        let result = await db.collection('comment').find({parent: new ObjectId(_id)}).toArray();
+        let result = await db.collection('comment').find({"parent": new ObjectId(_id)}).toArray();
         console.log(res.status(200).json(result))
     }else{
         console.log(res.status(400).json('잘못된 요청입니다.'))
