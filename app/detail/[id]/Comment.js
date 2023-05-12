682b9144363b3181bc12a8edc9aca8073d493b64
bug41 <bugkinguswit@gmail.com>
2023-05-12 22:43:02 +0900

    댓글 입력시 실시간 노출 수정

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 import dayjs from 'dayjs';
 
 export default function Comment(props){
@@ -8,47 +8,59 @@ export default function Comment(props){
     const [count, setCount] = useState(0);
 
     let [comment, setComment] = useState('')
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
     const today = dayjs().format('YYYY-MM-DD HH:mm:ss')
 
     //client component 에서 로드할땐 여기서 한다
-    useEffect(() => {
-        console.log('useEffect 호출')
-        fetch('/api/comment/list?_id='+props._id, {method: 'GET'})
-        .then(
-            response => response.json()
-        ).then(data => {
-            console.log('useEffect 호출 후 data 가져옴')
-          setData(data); // 가져온 데이터를 상태 변수에 저장
-        });
-    },[count])
+    useEffect(() => {       
+        fetchData();
+    },[count]);
+
+    const fetchData = useCallback(async () => {
+        try {
+            const response = await fetch('/api/comment/list?_id=' + props._id, { method: 'GET' });
+            const data = await response.json();
+
+            console.log(data);
+            setData(data); // 가져온 데이터를 상태 변수에 저장
+           
+        } catch (error) {
+            console.error('데이터 가져오기 실패:', error);
+        }
+    },[props._id]);
+
+    const handleCommentSubmit = async ()=>{
+        try{
+            await fetch('/api/comment/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    "parent" : props._id,
+                    "comment" : comment,
+                    "created_by" : today
+                })
+            });
+            setComment('');
+            setCount(prevCount => prevCount + 1);                    
+        }catch(error){  
+            console.error('댓글 등록에 실패하였습니다', error);
+        }       
+    };
 
     return (
         <div>
+            <hr/>
             <div>
-                {data ? (
-                    data.map((item, index) => (
+                {
+                data.length > 0 ? 
+                    data.map((item, index) => 
                      <div key={index}>{item.content}</div>
-                    ))
-                ) : (
-                    <div>Loading...</div>
-                )}
+                    )
+                    : 
+                '댓글없음'
+                }
             </div>
-            <input onChange={(e)=>{setComment(e.target.value)}} />
-            <button onClick={()=>{
-                //console.log(comment);
-                //fetch('/api/post/newComment', {method: 'POST', 
-                fetch('/api/comment/new', {method: 'POST', 
-                    body: JSON.stringify({
-                        "parent" : props._id,
-                        "comment" : comment,
-                        "created_by" : today
-                    })
-                }).then(
-                    setCount(count+1),
-                    console.log(count)
-                );
-            }}>댓글입력</button>
+            <input onChange={(e)=>{setComment(e.target.value)}} value={comment} />
+            <button onClick={handleCommentSubmit}>댓글입력</button>
         </div>
     )
 }
