d47061d744650573f81c0dae7402f36af4829bf8
bug41 <bugkinguswit@gmail.com>
2023-05-12 23:31:12 +0900

    loading.js error.js not-found.js

---
+++ app/detail/[id]/not-found.js
@@ -0,0 +1,5 @@
+export default function notFound(){
+    return (
+        <div>못 찾음1111111111111111</div>
+    )
+}
--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -5,14 +5,14 @@ import { ObjectId } from "mongodb";
 
 export default async function Detail(props) {  
   let db = (await connectDB).db('forum')
-  let result = await db.collection('post').findOne(
-    {
-      _id : new ObjectId(props.params.id)
-    }
-  );
+  let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.id) });
+
+  if(result === null){
+    return notFound()
+  }
   
   return (
-    <div>
+    <div className="p-20 detail">
       <h4>상세페이지임</h4>
       <h4>{result.title}</h4>
       <p>{result.content}</p>
