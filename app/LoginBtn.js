17ee825afd9ef5248ff37194c12f8119b93ed6c9
bug41 <bugkinguswit@gmail.com>
2023-05-15 21:02:40 +0900

    주석 추가

--- app/LoginBtn.js
+++ app/LoginBtn.js
@@ -1,9 +1,16 @@
 'use client'
 
 import {signIn, signOut} from 'next-auth/react'
+import { useEffect } from 'react'
 
 export default function LoginBtn() {
-    
+
+    //useEffect(()=>{
+    //    if(typeof window != 'undefined'){
+    //        localStorage.setItem('dartMode','값')
+    //    }
+    //},[])
+    //
     return(        
         <button onClick={()=>{signIn()}}>로그인</button>
     )
