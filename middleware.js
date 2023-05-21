25d781acf9fa37362e3d088e2b039d9f04eef8f6
bugkingus <bugkingus@gmail.com>
2023-05-22 08:53:53 +0900

    쿠키 추가

--- middleware.js
+++ middleware.js
@@ -3,6 +3,21 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(request) {
 
+  /* 
+  request.cookies.get('쿠키이름')  //출력
+  request.cookies.has('쿠키이름')  //존재확인
+  request.cookies.delete('쿠키이름')  //삭제
+  
+  const response = NextResponse.next()
+  response.cookies.set({
+    name: 'mode',
+    value: 'dark',
+    maxAge: 3600,
+    httpOnly : true
+  })  
+  return response  //쿠키생성
+  */  
+
   const session = await getToken({req: request})
   //console.log(session)
 
